da2526503486a93d6af98ec1467a2f5f9a63a42f
Kuni Natsuki <knatsuki92@gmail.com>
2015-04-06 20:00:37 -0700

    Frankenstein code combining all of our codes

--- client/src/app.js
+++ client/src/app.js
@@ -1,11 +1,42 @@
 var trackOld;
+var data = [
+{url: "audio/click.mp3", speed:2, port:0, recordedAtBpm: 120}, 
+{url: "audio/metronome2.mp3", speed:2, port:1, recordedAtBpm: 120}
+];
+
+
 $(function() {
   trackOld = {
     initialize: function(audioContext, bpm){
-      // this is the audio context
-      var AudioContext = window.AudioContext || window.webkitAudioContext;
-      var audioCtx = new AudioContext();
-      this.audioCtx = audioContext || audioCtx;
+
+        var contextClass = (window.AudioContext || 
+          window.webkitAudioContext || 
+          window.mozAudioContext || 
+          window.oAudioContext || 
+          window.msAudioContext);
+
+        if (contextClass) {
+          this.context = audioContext || new contextClass();
+          console.log('context created: ', this.context);
+
+          // Initialize a new instance of the BufferLoader class,
+          // passing in our context and data object. BufferLoader
+          // will buffer all of the recordings and hold onto
+          // references for the buffers.
+          this.bufferLoader = new BufferLoader(
+            this.context,
+            data
+            );
+
+          // Invoking bufferLoader's .load method does the actual
+          // buffering and loading of the recordings, and stores
+          // the buffers on the bufferloader instance.
+          this.bufferLoader.load();
+
+        } else {
+          // Web Audio API is not available. Ask the user to use a supported browser.
+          alert('Web Audio API is not available');
+        }
 
       this.tempoAdjustment = 0; //adjustment parameter when user changes tempo. initially set at 0.
       this.bpm  = bpm || 120;
@@ -46,7 +77,7 @@ $(function() {
     },
     pauseLoopNodeAnimation: function(loopNode, t){
       // t : the audioCtx time when mute event got triggered
-      t = t || this.audioCtx.currentTime;
+      t = t || this.context.currentTime;
       
       var disc = loopNode.d3Obj.container.select("circle.disc");
       disc.attr("class", "disc mute");
@@ -54,7 +85,7 @@ $(function() {
     },
     playLoopNodeAnimation: function(loopNode, t){
       // t : the audioCtx time when mute event got triggered
-      t = t || this.audioCtx.currentTime;
+      t = t || this.context.currentTime;
       
       var disc = loopNode.d3Obj.container.select("circle.disc");
       disc.attr("class", "disc unmute");
@@ -63,7 +94,7 @@ $(function() {
       // this needs to get cleaned up
 
       // t : the audioCtx time when record event got triggered
-      t = t || this.audioCtx.currentTime;
+      t = t || this.context.currentTime;
 
       var disc = loopNode.d3Obj.container.select("circle.disc");
       // put loopnode in wait mode
@@ -79,7 +110,7 @@ $(function() {
       var startPlayDeg = startRecordDeg + 360;
 
       d3.timer(function(){
-        var currentTime = this.audioCtx.currentTime;        
+        var currentTime = this.context.currentTime;        
         var currentDeg = (currentTime * speed - tempoAdjustment) / multiplier;
         console.log(currentDeg);
         if(currentDeg >= startRecordDeg){
@@ -93,7 +124,7 @@ $(function() {
 
       var startPlayTimer = function(callback){
         d3.timer(function(){
-          var currentTime = this.audioCtx.currentTime;        
+          var currentTime = this.context.currentTime;        
           var currentDeg = (currentTime * speed - tempoAdjustment) / multiplier;
           console.log(currentDeg);
           if(currentDeg >= startPlayDeg){
@@ -111,7 +142,7 @@ $(function() {
     changeTempo: function(bpm, t){
       // bpm : the new tempo
       // t : the audioCtx time when tempo changed
-      t = t || this.audioCtx.currentTime;
+      t = t || this.context.currentTime;
 
       this.tempoAdjustment = this.tempoAdjustment + t * (3/2) * (bpm - this.bpm);
       this.bpm = bpm;
@@ -122,7 +153,7 @@ $(function() {
     setCueAnimation: function(){
       d3.timer(function(){
         var loopNodes = this.loopNodes;
-        var audioCtxTime = this.audioCtx.currentTime;
+        var audioCtxTime = this.context.currentTime;
         var bar = calcBar(this.bpm);
         var speed = calcSpeed(bar);
         var tempoAdjustment = this.tempoAdjustment;
@@ -149,6 +180,121 @@ $(function() {
         }
       }.bind(this));
     },
+
+
+    play: function(button) {
+      // Grab the value associated with the button,
+      // will be used to identify the sound associated with the button.
+      var soundIndex = button.value;
+
+      // Grab the data object associated with the button.
+      var soundData = data[soundIndex];
+
+      console.log('playing a sound: ', soundData);
+
+      // Grab the amount of time a bar takes to complete.
+      var barTime = soundData.speed;
+
+      var currentTime = this.context.currentTime;
+
+      // The remainder tells us how much of the bartime we have 
+      // completed thus far.
+      var remainder = currentTime % barTime;
+      console.log('currentTime:', currentTime);
+
+      // The delay calculates how much we'll have to delay
+      // the playing of the sound so that we can perfectly
+      // match the time of the next beat.
+      var delay = barTime - remainder;
+      console.log('delay: ', delay);
+
+      console.log('expected time of play: ', delay + currentTime);
+
+      // Reassign the source associated with the soundData object
+      // to a new buffer source. This way, we can reference the same
+      // source to stop playing the loop. However, once stopped,
+      // the source is basically discarded and you must create
+      // a new one. One play per source.
+      var source = soundData.source = this.context.createBufferSource();
+      console.log('source', source);
+
+      // Associate the new source instance with the loaded buffer.
+      soundData.source.buffer = this.bufferLoader.bufferList[soundIndex];
+      soundData.source.loop = true;
+      // source.playbackRate.value = playbackControl.value;
+
+      // Create a gainNode, through which we will pass the soundData.
+      var gainNode = soundData.gainNode = this.context.createGain();
+      // Connect the source to the gainNode.
+      soundData.source.connect(gainNode);
+
+      //Connect the gainNode to the destination.
+      soundData.gainNode.connect(this.context.destination);
+
+      // Play the sound, delaying the sound by the delay necessary
+      // to make the sound play at the start of a new measure.
+      soundData.source.start(currentTime + delay);
+
+      // Define what parts of the soundfile will be played.
+      // In this case, second 0 to second barTime will be played.
+      soundData.source.loopStart = 0;
+      soundData.source.loopEnd = barTime;
+
+      source.onended = function() {
+        console.log('Your audio has finished playing');
+      };
+
+      console.log('source', source);
+    },
+
+    pause: function(button) {
+      var soundIndex = button.value;
+      var soundData = data[soundIndex];
+
+      // Instead of creating a new bufferSource as per usual,
+      // we retrieve the source that we have stored on our 
+      // data objects.
+      var source = soundData.source;
+      source.buffer = this.bufferLoader.bufferList[soundIndex];
+      console.log('About to pause, logging source: ', source);
+      source.stop();
+    },
+
+    toggle: function(button) {
+      console.log('button: ', button);
+      // var data = button.split(" ")
+      // console.log('data: ', data);
+      console.log(button.value);
+      if (button.innerHTML == "Play") {
+        button.innerHTML = "Pause";
+        this.play(button);
+      } else {
+        button.innerHTML = "Play";
+        this.pause(button);
+      }
+    },
+
+    changeVolume: function(slider, t){
+      var volume, soundIndex, soundData, gainNode;
+
+      volume = slider.value;
+      console.log('volume: ', volume);
+      soundIndex = $(slider).attr('soundIndex');
+      console.log('soundIndex: ', soundIndex);
+      soundData = data[soundIndex];
+      gainNode = soundData.gainNode;
+
+      gainNode.gain.value = volume / 100;
+      // slider : the new Volume
+      // t : the audioCtx time when Volume changed
+      // t = t || this.context.currentTime;
+
+      // this.volumeAdjustment = this.volumeAdjustment + t * (3/2) * (bpm - this.Params.bpm);
+      // this.Params.bpm = bpm;
+    },
+
+
+
     addListeners: function(){
       // Events
       this.loopNodes.forEach(function(loopNodeObj){
@@ -170,7 +316,7 @@ $(function() {
         this.changeTempo(480);
       }.bind(this));
 
-      $('#tempo').on('change', function(e){
+      $('#tempo').on('input', function(e){
         console.log(e.target.value);
         this.changeTempo(e.target.value);
       }.bind(this));
@@ -185,6 +331,26 @@ $(function() {
         this.recordLoopNodeAnimation(this.loopNodes[0]);
       }.bind(this));
 
+      //audio 
+
+      $('#volume0').on('input', function(e){
+        console.log(e.target.value);
+        this.changeVolume(e.target);
+      }.bind(this));
+
+      $('#volume1').on('input', function(e){
+        console.log(e.target.value);
+        this.changeVolume(e.target);
+      }.bind(this));
+
+      $('#sound1').on('click', function(e) {
+        this.toggle(e.target);
+      }.bind(this));
+
+      $('#sound2').on('click', function(e) {
+        this.toggle(e.target);
+      }.bind(this));
+
     }
   };
 });
@@ -196,7 +362,7 @@ var TrackModel = Backbone.Model.extend({
   setAudioContext: function(){ 
     var AudioContext = window.AudioContext || window.webkitAudioContext;
     var audioCtx = new AudioContext();
-    this.audioCtx = audioContext || audioCtx;
+    this.context = audioContext || audioCtx;
   }
 
 });
