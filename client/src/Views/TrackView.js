0c53cfa14304b2128ba90b856620283751e4e9b0
Kuni Natsuki <knatsuki92@gmail.com>
2015-04-13 18:46:04 -0700

    LoopNodeInfoView created

--- client/src/Views/TrackView.js
+++ client/src/Views/TrackView.js
@@ -1,9 +1,11 @@
 define([
   'text!templates/TrackViewTemplate.html',
   'Views/LoopNodesView',
+  'Views/LoopNodeInfoView',
   'Views/TrackInfoView'
 
-], function(template, LoopNodesView, TrackInfoView){
+
+], function(template, LoopNodesView, LoopNodeInfoView, TrackInfoView){
   var TrackView = Backbone.View.extend({
 
     initialize: function(){
@@ -20,10 +22,20 @@ define([
 
       // Append child views to corresponding class div
       // this.$el.find('.trackInfoView').append(new TrackInfoView().render().el);
-      // this.$el.find('.loopNodeInfoView').append(new LoopNodeInfoView().render().el);
+
+      var defaultLoopNode = this.model.get('loopNodes').models[0]; //setting default to first
+      this.updateLoopNodeInfoView(defaultLoopNode);
+
       // this.$el.find('.visualizerView').append(new VisualizerView().render().el);
-      this.$el.find('.loopNodesView').append(new LoopNodesView({collection: this.model.get('loopNodes')}).render().el);
       this.$el.find('.trackInfoView').append(new TrackInfoView({model: this.model}).render().el);
+      this.$el.find('.loopNodesView').append(new LoopNodesView({collection: this.model.get('loopNodes')}).render().el);
+
+
+      return this;
+    },
+
+    updateLoopNodeInfoView: function(loopNode){
+      this.$el.find('.loopNodeInfoView').html(new LoopNodeInfoView({ model: loopNode}).render().el);
       return this;
     }
     
