64d2dc40d066a592acf195ea02fea48c2f2264fa
Marc Balaban <marc@marcbalaban.com>
2015-04-20 16:14:44 -0700

    added id lookup for fetch

--- client/src/router.js
+++ client/src/router.js
@@ -27,26 +27,29 @@ define([
       // Track View page
       // Note: we need to set up ids.
 
-      // var audioData = [ {url: "/audio/dontlookback.mp3", multiplier: 8, recorded: true, speed:16, port: 1, recordedAtBpm: 120},
-      // {url: "/audio/handsonthewheel.mp3", recorded: true, multiplier: 8, speed:16, port: 2, recordedAtBpm: 120},
-      // {url: "/audio/rachelyamagata.mp3", recorded: true, multiplier: 8, speed:16, port: 3, recordedAtBpm: 120},
-      // {url: "/audio/silhouetteActiveChild.copy.mp3", recorded: true, multiplier: 8, speed:16, port: 4, recordedAtBpm: 120}
-      // ];
 
-      var audioData = [ {url: "", speed:2 , port: 1, recordedAtBpm: 120},
-      ];
+      $(function() {
+          $(".dial").knob({
+          });
+      });
+
+      if(id === "new"){
+        var audioData = [ {url: "../audio/metronome.mp3", speed:2, port: 1, recordedAtBpm: 120, recorded: true}];
+      } else {
+
+      }
+
       var track = new TrackModel({audioData: audioData});
       var trackView = new TrackView({model: track});
+
+
 
       $(".main").html(trackView.render().el);
       track.setCueAnimation();
 
       track.get('loopNodes').each(function(loopNode){loopNode.set('rerender', !loopNode.get('rerender'))})
 
-      $(function() {
-          $(".dial").knob({
-          });
-      });
+    
 
     },
     default: function(badUrl){
