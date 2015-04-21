44334ee86fa0206d332e1a4a75eee9394ed34989
Marc Balaban <marc@marcbalaban.com>
2015-04-20 18:58:05 -0700

    started on routing IDs.  Changed frontpage and added container to loopNodes.

--- client/src/router.js
+++ client/src/router.js
@@ -33,22 +33,24 @@ define([
           });
       });
 
+    var audioData = null
+      
       if(id === "new"){
-        var audioData = [ {url: "../audio/metronome.mp3", speed:2, port: 1, recordedAtBpm: 120, recorded: true}];
+       audioData =  [ {url: "../audio/metronome.mp3", speed:2, port: 1, recordedAtBpm: 120, recorded: true}];
       } else {
-
+        // Fetch
       }
 
       var track = new TrackModel({audioData: audioData});
       var trackView = new TrackView({model: track});
-
-
 
       $(".main").html(trackView.render().el);
       track.setCueAnimation();
 
       track.get('loopNodes').each(function(loopNode){loopNode.set('rerender', !loopNode.get('rerender'))})
 
+      $(".dial").knob();
+      
     
 
     },
