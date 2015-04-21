691ca6f5ecd7ce818dfdd564591cb6f2c8407519
SungMin <4sungmin@gmail.com>
2015-04-20 19:46:41 -0700

    Commit changes before I rebase master.

--- client/src/router.js
+++ client/src/router.js
@@ -27,7 +27,11 @@ define([
       // Track View page
       // Note: we need to set up ids.
 
-      var audioData = [ {url: "", speed:2, port: 1, recordedAtBpm: 120}];
+      var audioData = [ {url: "/audio/dontlookback.mp3", multiplier: 8, recorded: true, speed:16, port: 1, recordedAtBpm: 120},
+      {url: "/audio/handsonthewheel.mp3", recorded: true, multiplier: 8, speed:16, port: 2, recordedAtBpm: 120},
+      {url: "/audio/rachelyamagata.mp3", recorded: true, multiplier: 8, speed:16, port: 3, recordedAtBpm: 120},
+      {url: "/audio/silhouetteActiveChild.copy.mp3", recorded: true, multiplier: 8, speed:16, port: 4, recordedAtBpm: 120}
+      ];
       var track = new TrackModel({audioData: audioData});
       var trackView = new TrackView({model: track});
 
