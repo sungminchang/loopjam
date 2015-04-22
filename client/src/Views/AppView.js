1708db2c0b60256e05ede8c170a4939a599d45ca
SungMin <4sungmin@gmail.com>
2015-04-22 16:05:00 -0700

    [Features] 1. Create Recent Tracks view on main page with links that direct user to the relevant track. 2. Ask users whether they want to close or reload the tab.

--- client/src/Views/AppView.js
+++ client/src/Views/AppView.js
@@ -1,22 +1,80 @@
 define([
   'text!templates/AppViewTemplate.html',
-], function(template){
+  'text!templates/RecentTracksViewTemplate.html',
+], function(template, recentTracksTemplate){
   var AppView = Backbone.View.extend({
 
+    template: Handlebars.compile(template),
+
     initialize: function(){
-    },
+      // var $this = this;
+      // // Maybe I should do this at a certain time
+      // // Like when the other components have loaded
+      // $.ajax({
+      //   type: 'GET',
+      //   // url of the upload directory
+      //   url: '/tracks'
+      // }).done(function(data) {
+      //   console.log(data)
+      //   $this.$el.find('#table').bootstrapTable({
+      //     data: data,
+      //     columns: [{
+      //         field: 'trackname',
+      //         title: 'Track Name'
+      //       },
+      //       { field: 'id',
+      //         title: 'ID',
+      //       },
+      //       {
+      //         field: 'createdAt',
+      //         title: 'createdAt'
+      //       }]
+      //     });
+
+      //   // this.renderRecentTracks(data);
+      //   // log.innerHTML += "\n" + data;
+      //   });
+
+
+
+
+      },
 
     events:{
     },
 
-    template: Handlebars.compile(template),
-
     render: function() {
       // Load Template
       this.$el.html(this.template());
-
+      this.renderRecentTracks();
       return this;
     },
+
+    renderRecentTracks: function() {
+      this.$el.find('.recentTracks').children().detach();
+      var recentsTemplate = Handlebars.compile(recentTracksTemplate)
+      this.$el.find('.recentTracks').html(recentsTemplate());
+      var $this = this;
+      $.ajax({
+        type: 'GET',
+        // url of the upload directory
+        url: '/tracks'
+      }).done(function(data) {
+        var linkifyData = function(d) {
+          for (var i = 0; i < d.length; i++) {
+            var trackname = d[i]['trackname'].toString();
+            d[i]['trackname'] = '<a href="/#/tracks/' + trackname + '">' + trackname + ' </a> ';
+          }
+          return d;
+        };
+
+        var d = linkifyData(data);
+        $this.$el.find('#table').bootstrapTable({
+          data: d,
+          });
+      });
+    }
+
     
   });
 
