7794e5cf4b60d5c70aec7da11c445cddec09eee7
SungMin <4sungmin@gmail.com>
2015-04-23 18:58:47 -0700

    Working on passport implementation.

--- client/src/Views/AppView.js
+++ client/src/Views/AppView.js
@@ -17,6 +17,32 @@ define([
       // Load Template
       this.$el.html(this.template());
       this.renderRecentTracks();
+
+
+      $.ajax({
+        type: "POST",
+        url: "/tracks/:id",
+        data: {trackID: id},
+        success: function(data){
+          audioData =  JSON.parse(data.audioData);
+          for(var i = 0; i < audioData.length; i++){
+            audioData[i].port = i + 1;
+          }
+          var track = new TrackModel({audioData: audioData});
+          var trackView = new TrackView({model: track});
+
+        $this.mainView.renderTrackView(trackView);
+        track.setd3timer();
+        track.get
+
+
+      var signup = this.$el.find('#signup');
+      signup.on('click', function() {
+        $.ajax({
+          type: 'POST',
+          url: '/signup'
+        })
+      });
       return this;
     },
 
