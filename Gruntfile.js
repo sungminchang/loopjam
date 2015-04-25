33f4c9d52ecf2905e33cb874240a362b04047c23
Fahd <fahdnsheikh@gmail.com>
2015-04-24 17:29:47 -0700

    changed Chrome to PhantomJS

--- Gruntfile.js
+++ Gruntfile.js
@@ -86,7 +86,7 @@ module.exports = function (grunt) {
       travis: {
         configFile: 'karma.conf.js',
         singleRun: true,
-        browsers: ['Chrome']
+        browsers: ['PhantomJS']
       }
     },
     watch: {
