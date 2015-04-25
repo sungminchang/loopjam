49aa151ccd5091951fcdca07e1e7daeed9bb399b
Fahd <fahdnsheikh@gmail.com>
2015-04-24 17:32:52 -0700

    is false true? nein

--- test/test.js
+++ test/test.js
@@ -1,5 +1,5 @@
 describe('Sample test', function() {
     it('should not believe that !true is equal to true', function(){
-        expect(true).toBe(true);
+        expect(false).toBe(true);
       });
 });
