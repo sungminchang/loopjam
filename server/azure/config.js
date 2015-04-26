af3d67ea16e6e404567484371604ebbe44b7e943
Fahd <fahdnsheikh@gmail.com>
2015-04-25 17:07:34 -0700

    removed karma

---
+++ server/azure/config.js
@@ -0,0 +1,3 @@
+exports.AZR_USERNAME = process.env.AZR_USERNAME || "loopjammin";
+exports.AZR_KEY = process.env.AZR_KEY || "q+ylFl1iaLGQkFzl8W+9hhZL/5wIjRrxOoErEJ4bkPEEfAT/IR9qe1BLXzKrfKckDqa0iYRBmKsierCoAqcmAQ==";
+exports.AZR_HOST = process.env.AZR_HOST || "https://loopjammin.blob.core.windows.net/";
